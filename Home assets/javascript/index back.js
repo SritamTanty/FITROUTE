b202c5cdcd9fe4df8ea23cd6fee529c1d1ce1b16
SritamTanty <165694056+SritamTanty@users.noreply.github.com>
2024-06-24 12:37:51 +0530

    fitness management system
    it also include calorie and bmi calculator

---
+++ Home assets/javascript/index back.js
@@ -0,0 +1,138 @@
+//back body
+//glutes
+document.querySelector(".glutes1").addEventListener("mouseover", () => {
+    document.querySelector(".glutes1").style.fill = "red";
+    document.querySelector(".glutes2").style.fill = "red";
+});
+document.querySelector(".glutes2").addEventListener("mouseover", () => {
+    document.querySelector(".glutes1").style.fill = "red";
+    document.querySelector(".glutes2").style.fill = "red";
+});
+document.querySelector(".glutes2").addEventListener("mouseleave", () => {
+    document.querySelector(".glutes1").style.fill = "black";
+    document.querySelector(".glutes2").style.fill = "black";
+});
+
+document.querySelector(".glutes1").addEventListener("mouseleave", () => {
+    document.querySelector(".glutes1").style.fill = "black";
+    document.querySelector(".glutes2").style.fill = "black";
+});
+
+//hamstrings
+
+document.querySelector(".hamstrings1").addEventListener("mouseover", () => {
+    document.querySelector(".hamstrings1").style.fill = "red";
+    document.querySelector(".hamstrings2").style.fill = "red";
+});
+document.querySelector(".hamstrings2").addEventListener("mouseover", () => {
+    document.querySelector(".hamstrings1").style.fill = "red";
+    document.querySelector(".hamstrings2").style.fill = "red";
+});
+document.querySelector(".hamstrings2").addEventListener("mouseleave", () => {
+    document.querySelector(".hamstrings1").style.fill = "black";
+    document.querySelector(".hamstrings2").style.fill = "black";
+});
+
+document.querySelector(".hamstrings1").addEventListener("mouseleave", () => {
+    document.querySelector(".hamstrings1").style.fill = "black";
+    document.querySelector(".hamstrings2").style.fill = "black";
+});
+
+//lats
+
+document.querySelector(".lats1").addEventListener("mouseover", () => {
+    document.querySelector(".lats1").style.fill = "red";
+    document.querySelector(".lats2").style.fill = "red";
+});
+document.querySelector(".lats2").addEventListener("mouseover", () => {
+    document.querySelector(".lats1").style.fill = "red";
+    document.querySelector(".lats2").style.fill = "red";
+});
+document.querySelector(".lats2").addEventListener("mouseleave", () => {
+    document.querySelector(".lats1").style.fill = "black";
+    document.querySelector(".lats2").style.fill = "black";
+});
+
+document.querySelector(".lats1").addEventListener("mouseleave", () => {
+    document.querySelector(".lats1").style.fill = "black";
+    document.querySelector(".lats2").style.fill = "black";
+});
+
+//shoulders
+
+document.querySelector(".shoulders1").addEventListener("mouseover", () => {
+    document.querySelector(".shoulders1").style.fill = "red";
+    document.querySelector(".shoulders2").style.fill = "red";
+});
+document.querySelector(".shoulders2").addEventListener("mouseover", () => {
+    document.querySelector(".shoulders1").style.fill = "red";
+    document.querySelector(".shoulders2").style.fill = "red";
+});
+document.querySelector(".shoulders2").addEventListener("mouseleave", () => {
+    document.querySelector(".shoulders1").style.fill = "black";
+    document.querySelector(".shoulders2").style.fill = "black";
+});
+
+document.querySelector(".shoulders1").addEventListener("mouseleave", () => {
+    document.querySelector(".shoulders1").style.fill = "black";
+    document.querySelector(".shoulders2").style.fill = "black";
+});
+
+//calves
+
+document.querySelector(".calves1").addEventListener("mouseover", () => {
+    document.querySelector(".calves1").style.fill = "red";
+    document.querySelector(".calves2").style.fill = "red";
+});
+document.querySelector(".calves2").addEventListener("mouseover", () => {
+    document.querySelector(".calves1").style.fill = "red";
+    document.querySelector(".calves2").style.fill = "red";
+});
+document.querySelector(".calves2").addEventListener("mouseleave", () => {
+    document.querySelector(".calves1").style.fill = "black";
+    document.querySelector(".calves2").style.fill = "black";
+});
+
+document.querySelector(".calves1").addEventListener("mouseleave", () => {
+    document.querySelector(".calves1").style.fill = "black";
+    document.querySelector(".calves2").style.fill = "black";
+});
+
+//triceps
+
+document.querySelector(".triceps1").addEventListener("mouseover", () => {
+    document.querySelector(".triceps1").style.fill = "red";
+    document.querySelector(".triceps2").style.fill = "red";
+});
+document.querySelector(".triceps2").addEventListener("mouseover", () => {
+    document.querySelector(".triceps1").style.fill = "red";
+    document.querySelector(".triceps2").style.fill = "red";
+});
+document.querySelector(".triceps2").addEventListener("mouseleave", () => {
+    document.querySelector(".triceps1").style.fill = "black";
+    document.querySelector(".triceps2").style.fill = "black";
+});
+
+document.querySelector(".triceps1").addEventListener("mouseleave", () => {
+    document.querySelector(".triceps1").style.fill = "black";
+    document.querySelector(".triceps2").style.fill = "black";
+});
+
+//forearms
+
+document.querySelector(".forearms1").addEventListener("mouseover", () => {
+    document.querySelector(".forearms1").style.fill = "red";
+    document.querySelector(".forearms2").style.fill = "red";
+});
+document.querySelector(".forearms2").addEventListener("mouseover", () => {
+    document.querySelector(".forearms1").style.fill = "red";
+    document.querySelector(".forearms2").style.fill = "red";
+});
+document.querySelector(".forearms2").addEventListener("mouseleave", () => {
+    document.querySelector(".forearms1").style.fill = "black";
+    document.querySelector(".forearms2").style.fill = "black";
+});
+document.querySelector(".forearms1").addEventListener("mouseleave", () => {
+    document.querySelector(".forearms1").style.fill = "black";
+    document.querySelector(".forearms2").style.fill = "black";
+});
---
+++ Home assets/javascript/index.js
@@ -0,0 +1,158 @@
+document.querySelector(".chest1").addEventListener("mouseover", () => {
+    document.querySelector(".chest1").style.fill = "red";
+    document.querySelector(".chest2").style.fill = "red";
+});
+document.querySelector(".chest2").addEventListener("mouseover", () => {
+    document.querySelector(".chest1").style.fill = "red";
+    document.querySelector(".chest2").style.fill = "red";
+});
+document.querySelector(".chest2").addEventListener("mouseleave", () => {
+    document.querySelector(".chest1").style.fill = "black";
+    document.querySelector(".chest2").style.fill = "black";
+});
+
+document.querySelector(".chest1").addEventListener("mouseleave", () => {
+    document.querySelector(".chest1").style.fill = "black";
+    document.querySelector(".chest2").style.fill = "black";
+});
+
+//traps
+document.querySelector(".traps1").addEventListener("mouseover", () => {
+    document.querySelector(".traps1").style.fill = "red";
+    document.querySelector(".traps2").style.fill = "red";
+});
+document.querySelector(".traps2").addEventListener("mouseover", () => {
+    document.querySelector(".traps1").style.fill = "red";
+    document.querySelector(".traps2").style.fill = "red";
+});
+document.querySelector(".traps2").addEventListener("mouseleave", () => {
+    document.querySelector(".traps1").style.fill = "black";
+    document.querySelector(".traps2").style.fill = "black";
+});
+
+document.querySelector(".traps1").addEventListener("mouseleave", () => {
+    document.querySelector(".traps1").style.fill = "black";
+    document.querySelector(".traps2").style.fill = "black";
+});
+
+//shoulders
+
+document.querySelector(".shoulders1").addEventListener("mouseover", () => {
+    document.querySelector(".shoulders1").style.fill = "red";
+    document.querySelector(".shoulders2").style.fill = "red";
+});
+document.querySelector(".shoulders2").addEventListener("mouseover", () => {
+    document.querySelector(".shoulders1").style.fill = "red";
+    document.querySelector(".shoulders2").style.fill = "red";
+});
+document.querySelector(".shoulders2").addEventListener("mouseleave", () => {
+    document.querySelector(".shoulders1").style.fill = "black";
+    document.querySelector(".shoulders2").style.fill = "black";
+});
+
+document.querySelector(".shoulders1").addEventListener("mouseleave", () => {
+    document.querySelector(".shoulders1").style.fill = "black";
+    document.querySelector(".shoulders2").style.fill = "black";
+});
+
+//obliques
+
+document.querySelector(".obliques1").addEventListener("mouseover", () => {
+    document.querySelector(".obliques1").style.fill = "red";
+    document.querySelector(".obliques2").style.fill = "red";
+});
+document.querySelector(".obliques2").addEventListener("mouseover", () => {
+    document.querySelector(".obliques1").style.fill = "red";
+    document.querySelector(".obliques2").style.fill = "red";
+});
+document.querySelector(".obliques2").addEventListener("mouseleave", () => {
+    document.querySelector(".obliques1").style.fill = "black";
+    document.querySelector(".obliques2").style.fill = "black";
+});
+
+document.querySelector(".obliques1").addEventListener("mouseleave", () => {
+    document.querySelector(".obliques1").style.fill = "black";
+    document.querySelector(".obliques2").style.fill = "black";
+});
+
+//forearms
+
+document.querySelector(".forearms1").addEventListener("mouseover", () => {
+    document.querySelector(".forearms1").style.fill = "red";
+    document.querySelector(".forearms2").style.fill = "red";
+});
+document.querySelector(".forearms2").addEventListener("mouseover", () => {
+    document.querySelector(".forearms1").style.fill = "red";
+    document.querySelector(".forearms2").style.fill = "red";
+});
+document.querySelector(".forearms2").addEventListener("mouseleave", () => {
+    document.querySelector(".forearms1").style.fill = "black";
+    document.querySelector(".forearms2").style.fill = "black";
+});
+
+document.querySelector(".forearms1").addEventListener("mouseleave", () => {
+    document.querySelector(".forearms1").style.fill = "black";
+    document.querySelector(".forearms2").style.fill = "black";
+});
+
+//biceps
+
+document.querySelector(".biceps1").addEventListener("mouseover", () => {
+    document.querySelector(".biceps1").style.fill = "red";
+    document.querySelector(".biceps2").style.fill = "red";
+});
+document.querySelector(".biceps2").addEventListener("mouseover", () => {
+    document.querySelector(".biceps1").style.fill = "red";
+    document.querySelector(".biceps2").style.fill = "red";
+});
+document.querySelector(".biceps2").addEventListener("mouseleave", () => {
+    document.querySelector(".biceps1").style.fill = "black";
+    document.querySelector(".biceps2").style.fill = "black";
+});
+
+document.querySelector(".biceps1").addEventListener("mouseleave", () => {
+    document.querySelector(".biceps1").style.fill = "black";
+    document.querySelector(".biceps2").style.fill = "black";
+});
+
+//quads
+
+document.querySelector(".quads1").addEventListener("mouseover", () => {
+    document.querySelector(".quads1").style.fill = "red";
+    document.querySelector(".quads2").style.fill = "red";
+});
+document.querySelector(".quads2").addEventListener("mouseover", () => {
+    document.querySelector(".quads1").style.fill = "red";
+    document.querySelector(".quads2").style.fill = "red";
+});
+document.querySelector(".quads2").addEventListener("mouseleave", () => {
+    document.querySelector(".quads1").style.fill = "black";
+    document.querySelector(".quads2").style.fill = "black";
+});
+
+document.querySelector(".quads1").addEventListener("mouseleave", () => {
+    document.querySelector(".quads1").style.fill = "black";
+    document.querySelector(".quads2").style.fill = "black";
+});
+
+//calves
+
+document.querySelector(".calves1").addEventListener("mouseover", () => {
+    document.querySelector(".calves1").style.fill = "red";
+    document.querySelector(".calves2").style.fill = "red";
+});
+document.querySelector(".calves2").addEventListener("mouseover", () => {
+    document.querySelector(".calves1").style.fill = "red";
+    document.querySelector(".calves2").style.fill = "red";
+});
+document.querySelector(".calves2").addEventListener("mouseleave", () => {
+    document.querySelector(".calves1").style.fill = "black";
+    document.querySelector(".calves2").style.fill = "black";
+});
+
+document.querySelector(".calves1").addEventListener("mouseleave", () => {
+    document.querySelector(".calves1").style.fill = "black";
+    document.querySelector(".calves2").style.fill = "black";
+});
+
+
